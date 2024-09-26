5f41a47a555520dc4a301ac8454734909fe30001
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-09-26 13:40:58 +0700

    -.-

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   return NextResponse.json({
     ok: true,
-    fullName: "Dome Wick",
-    studentId: "660610999",
+    fullName: "Kachapat Punthong",
+    studentId: "660612137",
   });
 };
